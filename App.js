a78b82a0f8e775acbe43569201b99ce877223684
Rishabh Kukkar <rishabh.kukkar@cmile.in>
2021-03-11 18:31:54 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,42 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, {Component} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import WelcomePage from './src/Welcome';
+import SplashScreen from 'react-native-splash-screen';
+export default class App extends Component {
+  componentDidMount() {
+    // do stuff while splash screen is shown
+    // After having done stuff (such as async tasks) hide the splash screen
+    SplashScreen.hide();
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>Rishabh</Text>
+      </View>
+    );
+  }
+}
---
+++ src/Welcome.js
@@ -0,0 +1,19 @@
+import SplashScreen from 'react-native-splash-screen';
+
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+
+export default class WelcomePage extends Component {
+  componentDidMount() {
+    // do stuff while splash screen is shown
+    // After having done stuff (such as async tasks) hide the splash screen
+    SplashScreen.hide();
+  }
+  render() {
+    return (
+      <View>
+        <Text> Rishabh</Text>
+      </View>
+    );
+  }
+}
